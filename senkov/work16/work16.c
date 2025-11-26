ebcdbe2b91e3cd59e763cb3fd4910401e76a5127
dext01 <130887490+dext01@users.noreply.github.com>
2025-11-26 16:02:55 +0700

    Update work16.c

--- senkov/work16/work16.c
+++ senkov/work16/work16.c
@@ -5,32 +5,32 @@
 
 int main() {
     struct termios saved_attributes, new_attributes;
-    char ch;  // <--- ВОТ ЭТОЙ СТРОКИ НЕ ХВАТАЛО
+    char ch;
 
-    // Сохраняем старые настройки
+    // сохраняем старые настройки
     tcgetattr(STDIN_FILENO, &saved_attributes);
 
     new_attributes = saved_attributes;
 
-    // Отключаем ожидание Enter (канонический режим)
+    // отключаем ожидание Enter
     new_attributes.c_lflag &= ~ICANON; 
     
-    // Настраиваем чтение по 1 символу
+    // чтение по 1 символу
     new_attributes.c_cc[VMIN] = 1;
     new_attributes.c_cc[VTIME] = 0;
 
-    // Применяем новые настройки
+    // новые настройки
     tcsetattr(STDIN_FILENO, TCSANOW, &new_attributes);
 
     printf("Вы любите учиться и узнавать что-то новое, интересное для вас? (y/n)? ");
     fflush(stdout);
 
-    // Читаем символ
+    // читаем символ
     read(STDIN_FILENO, &ch, 1);
 
     printf("\nВы нажали: %c\n", ch);
 
-    // Возвращаем настройки как было (ОБЯЗАТЕЛЬНО!)
+    // возвращаем настройки как было
     tcsetattr(STDIN_FILENO, TCSANOW, &saved_attributes);
 
     return 0;
