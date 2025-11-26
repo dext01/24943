6233fc94accdb51a5d817c30ed902ca0ed00cb8a
dext01 <130887490+dext01@users.noreply.github.com>
2025-11-26 16:09:41 +0700

    Update work10.c

--- senkov/work10/work10.c
+++ senkov/work10/work10.c
@@ -1,26 +1,69 @@
-#include <sys/types.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
-int main(int argc, char *argv[]){
-    int status;
+int main(int argc, char *argv[])
+{
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <program> [args...]\n", argv[0]);
-        return 1;
+        fprintf(stderr, "Использование: %s <команда> [аргументы...]\n", argv[0]);
+        return EXIT_FAILURE;
     }
-    if (fork() == 0) {
+
+    printf("Parent: Preparing to launch command: %s\n", argv[1]);
+
+    // новый процесс
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        return EXIT_FAILURE;
+    }
+
+    // код для дочернего процесса
+    if (pid == 0) {
+        printf("Child [%d]: Executing command \"%s\"\n", getpid(), argv[1]);
+
+        // запускаем команду, которую передал пользователь.
+        // &argv[1] - это указатель на массив аргументов, начиная со второго элемента.
+        // сделано тк это позволяет передать саму команду одной структурой.
         execvp(argv[1], &argv[1]);
-        perror(argv[1]);
+
+        // Если программа дошла до этой строки, значит execvp не сработал (ошибка запуска)
+        perror("execvp");
         exit(127);
+    } 
+    // блок кода для родителя
+    else {
+        int status;
+
+        printf("Parent [%d]: Created child with pid %d\n", getpid(), pid);
+        printf("Parent: Waiting for child to finish...\n");
+
+        // блокируем родителя и ждем завершения дочернего процесса (pid)
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid");
+            return EXIT_FAILURE;
+        }
+
+        printf("\n--- Child Process Report ---\n");
+        
+        // как именно завершился ребенок
+        if (WIFEXITED(status)) {
+            // если процесс завершился нормально (через return или exit)
+            printf("Parent: Child exited normally with code: %d\n", WEXITSTATUS(status));
+        }
+        else if (WIFSIGNALED(status)) {
+            // если процесс был убит сигналом извне
+            printf("Parent: Child was killed by signal: %d\n", WTERMSIG(status));
+        }
+        else {
+            // (процесс остановлен, но не убит)
+            printf("Parent: Child stopped with status: 0x%x\n", status);
+        }
+
+        printf("Parent: Final message AFTER child has finished.\n");
     }
-    wait(&status);
-    if (WIFEXITED(status))
-        printf("exit status: %d\n", WEXITSTATUS(status));
-    else if (WIFSIGNALED(status))
-        printf("killed by signal: %d\n", WTERMSIG(status));
-    else
-        printf("child status: 0x%x\n", status);
+
     return 0;
 }
